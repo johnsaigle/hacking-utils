2b52ffa9a8ab1366f6ab07671b94c563e367d9c7
johnsaigle <john.saigle@gmail.com>
2020-11-01 14:30:22 -0500

    add ascii vals

---
+++ ascii-vals.py
@@ -0,0 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Get ASCII values from a payload')
+parser.add_argument('--javascript-payload', metavar='jsmode', type=bool, nargs='?', const=True, default=False,
+                    help='Print output wrapped in "<script>eval(String.fromCharCode(<PAYLOAD>))</script>')
+
+args = parser.parse_args()
+# string = sys.argv[1]
+string = input()
+
+x = [ord(char) for char in string]
+
+if args.javascript_payload:
+    print(f"<script>eval(String.fromCharCode{tuple(x)})</script>")
+else:
+    print(tuple(x))
